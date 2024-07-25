cd42ba388bbdd66b9bcc587defd55e2c449450a5
Adam Saponara <as@php.net>
2024-07-24 22:12:24 -0400

    send non-printable codepoints as U+FFFD

--- tests/test_invalid_utf8/test.php
+++ tests/test_invalid_utf8/test.php
@@ -2,7 +2,11 @@
 declare(strict_types=1);
 
 $test->ffi->tb_init();
-$test->ffi->tb_print_ex(0, 0, 0, 0, NULL, "foo\xc2\x00password");
+
+$y = 0;
+$test->ffi->tb_print_ex(0, $y++, 0, 0, NULL, "foo\xc2\x00password"); // stop at NULL
+$test->ffi->tb_set_cell(0, $y++, 0xffff, 0, 0); // invalid codepoint
+
 $test->ffi->tb_present();
 
 $test->screencap();
