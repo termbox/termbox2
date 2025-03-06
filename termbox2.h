e2ba83b266037ff4501cd102161a929a89f48ea7
AnErrupTion <anerruption@disroot.org>
2025-03-06 11:10:35 +0100

    Fix Unicode character support
    Signed-off-by: AnErrupTion <anerruption@disroot.org>

--- termbox2.h
+++ termbox2.h
@@ -1574,6 +1574,7 @@ static int bytebuf_reserve(struct bytebuf_t *b, size_t sz);
 static int bytebuf_free(struct bytebuf_t *b);
 
 int tb_init(void) {
+    setlocale(LC_CTYPE, "C.UTF-8"); // Required for iswprint(3) to work properly
     return tb_init_file("/dev/tty");
 }
 
